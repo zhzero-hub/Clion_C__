3959a29a71a7897b0434fdb1c2622e0cecd03fb8
zh <545033805@qq.com>
2019-11-02 21:59:24 +0800

    github test

---
+++ another test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by haoha on 2019/11/2.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    a.push_back(1);
+    cout << a.front();
+}
