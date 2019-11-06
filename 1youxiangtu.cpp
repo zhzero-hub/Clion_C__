c3a92a7aa1c89c37ca457c59beb70d44f70b1bdb
zhzero <57288044+zhzero-hub@users.noreply.github.com>
2019-11-06 15:09:23 +0800

    上机版本

--- 1youxiangtu.cpp
+++ 1youxiangtu.cpp
@@ -10,20 +10,24 @@
 
 
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <unordered_set>
+#include <fstream>
+#include <vector>
+using namespace std;
 typedef struct arcnode
 {
-    int index;//邻接点序号
-    struct arcnode *next;
+    int index = -1;//邻接点序号
+    struct arcnode *next = nullptr;
 }AR;
 
 typedef struct MyGraph
 {
-    int type;//0，表示无向图，1表示有向图
-    int arcnum,vexnum;//图中边的个数以及顶点个数
-    char **vexname;//存放顶点名的二维数组
+    int type = -1;//0，表示无向图，1表示有向图
+    int arcnum = 0,vexnum = 0;//图中边的个数以及顶点个数
+    char *vexname[20];//存放顶点名的二维数组
     AR *N;//存放邻接表头结点的数组
 }GH;
 
@@ -52,7 +56,7 @@ int menu_select()
     return (i-'0');
 }
 
-main()
+int main()
 {
     GH G;
     char c1[20],c2[20];
@@ -108,16 +112,44 @@ int findvex(char *s,GH *G)//根据顶点名确定该顶点的编号
         if(strcmp(s,G->vexname[i])==0)
             return i;
     }
-    printf("read file erro\n");
+    printf("read file error\n");
     exit(-1);
 }
 
 void creatgraph(GH *G)
 {
-////////////////////////完成该函数///////////////////////////////////////////////
     char filename[]="graph1.txt";
-
-
+    ifstream file(filename);
+    if(file.fail())
+    {
+        cout << "文件打开失败" << endl;
+        exit(0);
+    }
+    G->type = 0;
+    file >> G->vexnum;
+    G->N = (AR *)malloc(G->vexnum * sizeof(AR));
+    for(int i = 0;i < G->vexnum;i ++)
+    {
+        G->vexname[i] = (char *)malloc(20 * sizeof(char));
+        file >> G->vexname[i];
+        G->N[i].index = i;
+        G->N[i].next = nullptr;
+    }
+    while(!file.eof())
+    {
+        char p[20] , q[20];
+        file >> p >> q;
+        int pos = findvex(p , G);
+        int data = findvex(q , G);
+        AR *x = &G->N[pos];
+        while(x->next != nullptr)x = x->next;
+        AR *temp = (AR *)malloc(sizeof(AR));
+        temp->next = nullptr;
+        temp->index = data;
+        x->next = temp;
+        G->arcnum ++;
+    }
+    file.close();
 }
 
 
@@ -139,24 +171,88 @@ void showgraph(GH *G)
     printf("\n");
 }
 
+void Find(GH *G , char *start , AR *x , int *visit , vector<char *> &path)
+{
+    if(strcmp(G->vexname[x->index] , start) == 0)
+    {
+        for(const auto &y: path)cout << y << "----";
+        cout << start << endl;
+    }
+    if(visit[x->index] == 0)
+    {
+        visit[x->index] = 1;
+        path.push_back(G->vexname[x->index]);
+        Find(G , start , &G->N[x->index] , visit , path);
+        visit[x->index] = 0;
+        path.pop_back();
+    }
+    if(x->next != nullptr)
+    {
+        x = x->next;
+        Find(G , start , x , visit , path);
+    }
+}
+
 
 void findpath(GH *G,char *start)//寻找所有经过顶点start的简单回路
 {
-////////////////////////完成该函数///////////////////////////////////////////////
-
-
+    int visit[20] = {};
+    vector<char *> path;
+    int pos = findvex(start , G);
+    visit[pos] = 1;
+    path.push_back(start);
+    AR *x = &G->N[pos];
+    x = x->next;
+    if(x == nullptr)cout << "该点为孤立点" << endl;
+    Find(G , start , x , visit , path);
 }
 
 void indegree(GH *G)//计算并显示输出图中每个顶点的入度
 {
-////////////////////////完成该函数///////////////////////////////////////////////
+    int ans[20] = {};
+    for(int i = 0;i < G->vexnum;i ++)
+    {
+        AR *x = &G->N[i];
+        x = x->next;
+        while(x != nullptr)
+        {
+            ans[x->index] ++;
+            x = x->next;
+        }
+    }
+    for(int i = 0;i < G->vexnum;i ++)cout << G->vexname[i] << "的入度为: " << ans[i] << endl;
+}
 
+void find(GH *G , char *end , AR *x , int *visit , int &ans)
+{
+    if(strcmp(G->vexname[x->index] , end) == 0)
+    {
+        ans = 1;
+        return;
+    }
+    if(!ans && visit[x->index] == 0)
+    {
+        visit[x->index] = 1;
+        find(G , end , &G->N[x->index] , visit , ans);
+        visit[x->index] = 0;
+    }
+    if(!ans && x->next != nullptr)
+    {
+        x = x->next;
+        find(G , end , x , visit , ans);
+    }
 }
 
 int ispath(GH *G,char *start,char *end)//判断从顶点start到顶点end是否存在路径，存在返回1，否则返回0
 {
-///////////////////////完成该函数///////////////////////////////////////////////
-
+    int visit[20] = {};
+    int pos = findvex(start , G);
+    visit[pos] = 1;
+    AR *x = &G->N[pos];
+    x = x->next;
+    int ans = 0;
+    find(G , end , x , visit , ans);
+    return ans;
 }
 
 
